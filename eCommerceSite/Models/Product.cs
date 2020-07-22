d07484722da5bd978af1ec813b5a57aead289e6b
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-07-22 16:23:16 -0700

    Made Product class and gave it properties

---
+++ eCommerceSite/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    /// <summary>
+    /// Represents individual product
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Unique ID number for product
+        /// </summary>
+        [Key] // makes primary key in database
+        public int ProductId { get; set; }
+        /// <summary>
+        /// Consumer name of the product
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// The retail price as US currency
+        /// </summary>
+        public double Price { get; set; }
+        /// <summary>
+        /// Category products fall under ex. Electronics, Furniture, etc...
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
