6ce03f7d2d6a648a06d6d73b3a454b727312e55c
Spencer Sterling <63821532+SpencerTSterling@users.noreply.github.com>
2020-08-05 22:45:49 -0700

    Issue#5 add product (#14)
    * Adding product is implemented
    
    * Added bootstrap/refactored success message
    
    * Refactored DB code to async

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceSite.Controllers
 {
@@ -12,7 +13,7 @@ namespace eCommerceSite.Controllers
     {
         private readonly ProductContext _context;
 
-        public ProductController (ProductContext context)
+        public ProductController(ProductContext context)
         {
             _context = context;
         }
@@ -21,13 +22,38 @@ namespace eCommerceSite.Controllers
         /// Displays a veiw that lists all products
         /// </summary>
         /// <returns></returns>
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             //Get all products out of database
-            List<Product> products = _context.Products.ToList();
+            List<Product> products = await _context.Products.ToListAsync();
 
             //Send list of products to view to be displayed
             return View(products);
         }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Product p)
+        {
+            if(ModelState.IsValid)
+            {
+                //Add to DB
+                _context.Products.Add(p);
+                await _context.SaveChangesAsync();
+
+                // success message
+                TempData["Message"] = $"{p.Title} was added successfully";
+
+                // redirect to catalog page
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
