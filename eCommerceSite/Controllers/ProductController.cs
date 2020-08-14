fec2340e9f36d51129ab007ed479a66f9e87c6eb
Spencer Sterling <63821532+SpencerTSterling@users.noreply.github.com>
2020-08-13 21:09:18 -0700

    Edit product implemented (#20)

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -55,5 +55,29 @@ namespace eCommerceSite.Controllers
             return View();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            // Get product with corressponding id
+            Product p =
+                await (from prod in _context.Products
+                       where prod.ProductId == id
+                       select prod).SingleAsync();
+
+            // Pass product to view
+            return View(p);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit (Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(p).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                ViewData["Message"] = "Product updated successfully";
+            }
+            return View(p);
+        }
+
     }
 }
