9dd92363f57926953857701668144e7b06f09109
Spencer Sterling <63821532+SpencerTSterling@users.noreply.github.com>
2020-09-01 18:31:30 -0700

    Issue#28 shopping cart (#41)
    * Add shopping cart icon to navigation (links to catalog)
    
    * Added ShoppingCart link to navigation and Add to Cart button in catalog
    
    * GetProductAsync refactored method in ProductDb
    
    * Added IHttpContextAccessor feild + passed it into constructor to access sessions/cookies
    
    * Single product can be put into cart
    
    * Eable to store multiple items in the shopping cart
    
    * Created basic summary page (requires list)
    
    * Refactored CartController
    
    * Added a message for an empty cart

---
+++ eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ProductContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+        public CartController(ProductContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        /// <summary>
+        /// Add the product to the shopping cart
+        /// </summary>
+        /// <param name="id">ProductId</param>
+        /// <returns></returns>
+        public async Task<IActionResult> Add(int id) // id of product to add
+        {
+            // get product from data base
+            Product p = await ProductDb.GetProductAsync(_context, id);
+
+            CookieHelper.AddProductToCart((HttpContextAccessor)_httpContext, p);
+
+            // redirect to previous page
+            return RedirectToAction("Index", "Product");
+        }
+
+        public IActionResult Summary()
+        {
+            //Display all products in the shopping cart
+
+            List<Product> cartProducts = CookieHelper.GetCartProducts(_httpContext);
+            return View(cartProducts);
+        }
+    }
+}
